2a1246a61866cedf1f700956141f60b7a92bd66a
gilles <gilles1348@gmail.com>
2023-06-05 20:49:24 +0200

    Initial commit

---
+++ src/main/java/com/gilles_m/rpgregen/RPGRegen.java
@@ -0,0 +1,17 @@
+package com.gilles_m.rpgregen;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RPGRegen extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
